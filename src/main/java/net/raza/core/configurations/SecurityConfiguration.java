97483ee7f36b3c01aeb4954b42f02dbaf703905f
Tassio Abreu de Souza <tazabreu@gmail.com>
2016-03-08 03:01:00 -0300

    Basic authentication impl.

--- src/main/java/net/raza/core/configurations/SecurityConfiguration.java
+++ src/main/java/net/raza/core/configurations/SecurityConfiguration.java
@@ -1,21 +1,34 @@
 package net.raza.core.configurations;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
-@EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity.authorizeRequests().antMatchers("/").permitAll().and()
-                .authorizeRequests().antMatchers("/console/**").permitAll();
-
-        httpSecurity.csrf().disable();
-        httpSecurity.headers().frameOptions().disable();
+        httpSecurity
+        	.authorizeRequests()
+        		.antMatchers("/home", "/", "/css/**", "/js/**", "/resources/**").permitAll()
+            	.anyRequest().authenticated()
+            	.and()
+        	.formLogin()
+            	.loginPage("/login")
+            	.permitAll()
+            	.and()
+        	.logout()
+            	.permitAll();
+    }
+    
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+            .inMemoryAuthentication()
+                .withUser("user").password("password").roles("USER");
     }
 
 }
