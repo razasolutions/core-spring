f21f016539348ccf2a7db040196b4a5551ff8a6e
Tassio Abreu de Souza <tazabreu@gmail.com>
2016-03-11 22:14:11 -0300

    Bugfixes for User editing feature.

--- src/main/java/net/raza/core/controllers/UserController.java
+++ src/main/java/net/raza/core/controllers/UserController.java
@@ -43,7 +43,7 @@ public class UserController {
     	return "redirect:" + PATH + "/userManagement";
     }
     
-    @RequestMapping(value = PATH + "/userManagement/edit/", method = RequestMethod.POST)
+    @RequestMapping(value = PATH + "/userManagement/edit", method = RequestMethod.POST)
     public String editUser(User managedUser){
     	userService.save(managedUser);
     	// TODO: add a success message into Flash context;
